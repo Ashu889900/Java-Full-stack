afe8108003ce3cf4d75f88fc144ef8efe292d33d
Ashu889900 <akje269@gmail.com>
2026-01-06 21:33:38 +0530

    Prepared Statements in JDBC

--- JDBC/DemoJDBC/src/JdbcClass.java
+++ JDBC/DemoJDBC/src/JdbcClass.java
@@ -13,15 +13,16 @@ public class JdbcClass {
          *  CLOSE CONNECTION
          */
 
-         int sid = 5;
-         String sname = "Robert";
-         int marks = 78;
+         int sid = 6;
+         String sname = "thomas";
+         int marks = 98;
 
          String url = "jdbc:postgresql://localhost:5432/postgres";
          String uname = "postgres";
          String password = "0000";
 //         String sql = "select * from student;";
-         String sql = "Insert into student values("+ sid +", '"+ sname +"', "+ marks +");";
+//         String sql = "Insert into student values("+ sid +", '"+ sname +"', "+ marks +");";
+         String sql = "Insert into student values(?, ?, ?);";
 //         String sql = "Update student set sname = 'pera pera' where sid = 5;";
 //         String sql = "delete from student where sid = 5;";
 
@@ -32,7 +33,11 @@ public class JdbcClass {
          }
          try {
              Connection con = DriverManager.getConnection(url, uname, password);
-             Statement st = con.createStatement();
+//             Statement st = con.createStatement();
+             PreparedStatement st = con.prepareStatement(sql);
+             st.setInt(1,sid);
+             st.setString(2,sname);
+             st.setInt(3,marks);
 //             ResultSet rs = st.executeQuery(sql);
 //             rs.next();
 //             System.out.println(rs.getString("sname"));
@@ -42,7 +47,7 @@ public class JdbcClass {
 //                 System.out.println(rs.getInt("marks"));
 //             }
 
-             boolean status =  st.execute(sql);
+             boolean status =  st.execute();
              System.out.println(status);
 
 //             rs.close();
